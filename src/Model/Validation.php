73b1a8ec05383f5be9f42b77a56a148dcfa6e14b
SaymonG <105525945+SaymonG@users.noreply.github.com>
2022-05-26 20:54:02 -0300

    Add files via upload

---
+++ src/Model/Validation.php
@@ -0,0 +1,27 @@
+<?php
+//Bernardo--
+//criação de classe de validação 
+namespace APP\Model;
+class Validation
+{
+    //criação das requisições que serão cobradas no client.php
+    public static function validateId(int $id)
+    {
+        return $id > 0;
+    }
+    public static function validateName(string $name): bool
+    {
+        return strlen($name) > 2 && !is_numeric($name);
+    }
+
+    public static function validatePhone(string $phone)
+    {
+        return preg_match("/^\(?\d{2}\)?[\s-]?\d{5}-?\d{4}$/", $phone);
+    }
+
+    public static function validateEmail(string $email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+}
+?>
